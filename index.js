7465a86377b613e1b26beec6754cde962b681884
vhayne14 <vhayne14@gmail.com>
2021-09-03 17:02:37 +1000

    Added comments line 24

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.set('views', './templates')
 
 // MIDDLEWARE ROUTES
 
+// GET USER 
+/*
+    Bacon ipsum dolor amet capicola sausage pork loin hamburger buffalo
+     beef filet mignon jerky swine tongue pork. Beef fatback jowl, ribeye
+      frankfurter flank corned beef biltong prosciutto drumstick hamburger
+       ham hock spare ribs short ribs.
+*/
 
 
 app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}`))
