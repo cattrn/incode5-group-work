bcad05650272a42d699df7d30a61e21043432e75
Shubham <69906688+shubhamb04@users.noreply.github.com>
2021-09-03 19:13:40 +1000

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,39 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {
-  check,
-  expressValidator,
-  validationResult,
-} = require("express-validator");
 
 router.post(
   "/users",
-  [
-    check("firstname", "First Name must be 3 or more characters!")
-      .exists()
-      .isLength({ min: 3 }),
-    check("lastname", "Last Name must be 3 or more characters!")
-      .exists()
-      .isLength({ min: 3 }),
-    check("email", "Please enter valid email address!")
-      .exists()
-      .isEmail()
-      .normalizeEmail(),
-    check("password", "Password must be atleast 8 characters")
-      .exists()
-      .isLength({ min: 8 }),
-  ],
   async (req, res) => {
-    const errors = validationResult(req);
+
     const { firstname, lastname, email, password } = req.body;
     const hash = await bcrypt.hash(password, 10);
-
-    if (!errors.isEmpty()) {
-      const errorMsgs = errors.array();
-      res.render("pages/add_user", {
-        errorMsgs,
-      });
-    }
 
     data.users.push({
       firstname: firstname,
